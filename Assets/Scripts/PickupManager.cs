0c09fbdb6f0bc4546a7d11fdee880da0670839e6
Munchlaxlol <niklas.landborg@elev.ga.lbs.se>
2024-01-09 16:15:43 +0100

    inte klar

---
+++ Assets/Scripts/PickupManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PickupManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pickUp1;
+
+
+    int holding = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            PickUp();
+            holding++;
+        }
+
+ 
+    }
+    void PickUp()
+    {
+            bool hold1 = false;
+
+            hold1 = true;
+            while (hold1)
+            {
+                pickUp1.transform.position = transform.position + new Vector3(1, 1, 0);
+                if (Input.GetKey(KeyCode.Q))
+                {
+                    hold1 = false;
+                    break;
+                    holding--;
+                }
+            }
+
+        
+    }
+}
