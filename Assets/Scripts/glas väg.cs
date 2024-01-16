94f3a47a9451e35c73d999acfad8b516352e88ae
Tuddebud <150425823+Tuddebud@users.noreply.github.com>
2024-01-16 14:14:28 +0100

    door

---
+++ Assets/Scripts/glas väg.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glasväg : MonoBehaviour
+{
+    // Start is called before the first frame update'
+    public void Destroywindow()
+    {
+
+        Destroy(gameObject);
+
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
