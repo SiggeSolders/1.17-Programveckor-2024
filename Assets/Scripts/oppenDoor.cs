5160654a9c7e44b141447d226d3ef90eaa2116f1
Tuddebud <150425823+Tuddebud@users.noreply.github.com>
2024-01-22 12:33:19 +0100

    dör nu ser bra :D

---
+++ Assets/Scripts/oppenDoor.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Difrent get component
+        PickupScript pickup = gameObject.GetComponent<PickupScript>();
+        GameObject otherGameObject = collision.gameObject;
+        LLLock hitLock = otherGameObject.GetComponent<LLLock>();
+        glasväg hitglas = otherGameObject.GetComponent<glasväg>();
+        GameObject play = collision.gameObject;
+
+        //Send signal to door to run door script
+        if (pickup.holdingKey == true)
+        {
+            if (hitLock != null)
+            {
+                hitLock.unlock();
+
+            }
+        }
+
+        //Send signal to glas to run glas wall script
+        if (pickup.holdingPipe == true)
+        {
+            if (hitglas != null)
+            {
+                hitglas.Destroywindow();
+                pickup.holdPipe = false;
+
+            }
+        }
+
+
+    }
+}
