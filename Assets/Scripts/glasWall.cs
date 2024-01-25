ea8b96862d951e33a57e2e74200f96dbf87aa6bb
Tuddebud <150425823+Tuddebud@users.noreply.github.com>
2024-01-25 12:53:34 +0100

    glas vägg nice

--- Assets/Scripts/glasWall.cs
+++ Assets/Scripts/glasWall.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class glasväg : MonoBehaviour
+public class glasWall : MonoBehaviour
 {
     // Start is called before the first frame update'
     [SerializeField]
